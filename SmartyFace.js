6671fdf462527037d9a9bbb417a2bcb6dc4fd136
Ben Cox <Ben@Benjojo.co.uk>
2013-11-05 22:22:47 +0000

    First commit, laying out the file itself

---
+++ SmartyFace.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name                SmartyFace
+// @description        Text Prediction on facepunch
+// @author                benjojo
+// @namespace        http://facepunch.com
+// @include                http://facepunch.com/*
+// @include         http://www.facepunch.com/*
+// @include                https://facepunch.com/*
+// @include         https://www.facepunch.com/*
+// @version         1
+// ==/UserScript==
+
+
+var endPoint = "http://text.nsa.me.uk/pose";
