815beeba01b3c2336a9bc3d799a87e924cf8c961
Ben Cox <Ben@Benjojo.co.uk>
2013-11-05 23:57:17 +0000

    Remove useless item

--- SmartyFace.js
+++ SmartyFace.js
@@ -24,8 +24,6 @@ $( document ).ready(function() {
                         $.post( endPoint, { post: text }, function( data ) {
                                 $('#SmartyFace').html( $(textarea).val()+data );
                         });
-                        // $('#SmartyFace').html($(textarea).val());
-
                 });
         }
         findTextBoxes();
