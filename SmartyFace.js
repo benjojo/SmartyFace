c68e40f0e90f8a8480ff52f07f8c47b1e0a492fe
Ben Cox <Ben@Benjojo.co.uk>
2013-11-05 22:53:19 +0000

    The script now hooks to text boxes.

--- SmartyFace.js
+++ SmartyFace.js
@@ -12,3 +12,15 @@
 
 
 var endPoint = "http://text.nsa.me.uk/pose";
+
+function findTextBoxes (argument) {
+        var boxes = $('textarea');
+        var parent = boxes[2].parentNode;
+        $(parent).prepend("<i id=\"SmartyFace\" style=\"color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">This is a test wow. such test</i>");
+        var textarea = boxes[2];
+        $(boxes[2]).keypress(function(a) {
+
+                $('#SmartyFace').html($(textarea).val());
+        });
+}
+findTextBoxes();
