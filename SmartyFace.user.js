9c1a9c99ae631f64202a6a249457f32482c2db3c
Ben Cox <Ben@Benjojo.co.uk>
2013-11-06 00:23:44 +0000

    Okay what the hell, Why is it so suddenly hard to get stuff working.
    ~webdev~

--- SmartyFace.user.js
+++ SmartyFace.user.js
@@ -9,22 +9,41 @@
 // @include         https://www.facepunch.com/*
 // @version         1
 // ==/UserScript==
+var s=document.createElement('script');
+s.setAttribute('type','text/javascript');
+s.innerHTML='$( document ).ready(function() { \
+        var endPoint = "http://text.nsa.me.uk/pose"; \
+        function findTextBoxes (argument) { \
+                var boxes = $("textarea"); \
+                var parent = boxes[2].parentNode; \
+                $(parent).prepend("<i id=\"SmartyFace\" style=\"pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">Type Reply Here</i>"); \
+                var textarea = boxes[2]; \
+                $(boxes[2]).keypress(function(a) { \
+                        var text = $(textarea).val(); \
+                        $.post( endPoint, { post: text }, function( data ) { \
+                                $("#SmartyFace").html( $(textarea).val()+data ); \
+                        }); \
+                }); \
+        } \
+        findTextBoxes(); \
+});';
+var a = document.body.appendChild(s);
+console.log(a);
 
+// $( document ).ready(function() {
+//         var endPoint = "http://text.nsa.me.uk/pose";
 
-$( document ).ready(function() {
-        var endPoint = "http://text.nsa.me.uk/pose";
-
-        function findTextBoxes (argument) {
-                var boxes = $('textarea');
-                var parent = boxes[2].parentNode;
-                $(parent).prepend("<i id=\"SmartyFace\" style=\"pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">Type Reply Here</i>");
-                var textarea = boxes[2];
-                $(boxes[2]).keypress(function(a) {
-                        var text = $(textarea).val();
-                        $.post( endPoint, { post: text }, function( data ) {
-                                $('#SmartyFace').html( $(textarea).val()+data );
-                        });
-                });
-        }
-        findTextBoxes();
-});
+//         function findTextBoxes (argument) {
+//                 var boxes = $('textarea');
+//                 var parent = boxes[2].parentNode;
+//                 $(parent).prepend("<i id=\"SmartyFace\" style=\"pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">Type Reply Here</i>");
+//                 var textarea = boxes[2];
+//                 $(boxes[2]).keypress(function(a) {
+//                         var text = $(textarea).val();
+//                         $.post( endPoint, { post: text }, function( data ) {
+//                                 $('#SmartyFace').html( $(textarea).val()+data );
+//                         });
+//                 });
+//         }
+//         findTextBoxes();
+// });
