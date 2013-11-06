fe958ec75b736e678e0969cbed8dab9db6bdcb8b
Ben Cox <Ben@Benjojo.co.uk>
2013-11-06 00:31:27 +0000

    F this going to bed

--- SmartyFace.user.js
+++ SmartyFace.user.js
@@ -11,22 +11,7 @@
 // ==/UserScript==
 var s=document.createElement('script');
 s.setAttribute('type','text/javascript');
-s.innerHTML='$( document ).ready(function() { \
-        var endPoint = "http://text.nsa.me.uk/pose"; \
-        function findTextBoxes (argument) { \
-                var boxes = $("textarea"); \
-                var parent = boxes[2].parentNode; \
-                $(parent).prepend("<i id=\"SmartyFace\" style=\"pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">Type Reply Here</i>"); \
-                var textarea = boxes[2]; \
-                $(boxes[2]).keypress(function(a) { \
-                        var text = $(textarea).val(); \
-                        $.post( endPoint, { post: text }, function( data ) { \
-                                $("#SmartyFace").html( $(textarea).val()+data ); \
-                        }); \
-                }); \
-        } \
-        findTextBoxes(); \
-});';
+s.innerHTML='$(document).ready(function(){(function(a){a=$(".cke_enable_context_menu");console.log(a);$(a[0].parentNode).prepend(\'<i id="SmartyFace" style="pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;">Type Reply Here</i>\');var b=a[0];$(a[0]).keypress(function(a){a=$(b).val();$.post("http://text.nsa.me.uk/pose",{post:a},function(a){$("#SmartyFace").html($(b).val()+a)})})})()});';
 var a = document.body.appendChild(s);
 console.log(a);
 
@@ -35,10 +20,10 @@ console.log(a);
 
 //         function findTextBoxes (argument) {
 //                 var boxes = $('textarea');
-//                 var parent = boxes[2].parentNode;
+//                 var parent = boxes[0].parentNode;
 //                 $(parent).prepend("<i id=\"SmartyFace\" style=\"pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">Type Reply Here</i>");
-//                 var textarea = boxes[2];
-//                 $(boxes[2]).keypress(function(a) {
+//                 var textarea = boxes[0];
+//                 $(boxes[0]).keypress(function(a) {
 //                         var text = $(textarea).val();
 //                         $.post( endPoint, { post: text }, function( data ) {
 //                                 $('#SmartyFace').html( $(textarea).val()+data );
