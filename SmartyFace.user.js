bd610358ffcb78919a42a4f532604d810ac49f10
Ben Cox <Ben@Benjojo.co.uk>
2013-11-06 00:49:07 +0000

    Oooo, Now we are getting somewhere

--- SmartyFace.user.js
+++ SmartyFace.user.js
@@ -11,7 +11,7 @@
 // ==/UserScript==
 var s=document.createElement('script');
 s.setAttribute('type','text/javascript');
-s.innerHTML='$(document).ready(function(){function b(a){a=$(".cke_enable_context_menu");$(a[0].parentNode).prepend(\'<i id="SmartyFace" style="pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;">Type Reply Here</i>\');var b=a[0];$(a[0]).keypress(function(a){a=$(b).val();$.post(c,{post:a},function(a){$("#SmartyFace").html($(b).val()+a)})})}var c="http://text.nsa.me.uk/pose";setInterval(b,1500);setInterval(b,15E3)});';
+s.innerHTML='$(document).ready(function(){function b(a){a=$(".cke_enable_context_menu");if(a[0]){$(a[0].parentNode).prepend(\'<i id="SmartyFace" style="pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;">Type Reply Here</i>\');var c=a[0];$(a[0]).keypress(function(a){a=$(c).val();$.post(d,{post:a},function(a){$("#SmartyFace").html($(c).val()+a)})})}else setInterval(b,1500)}var d="http://text.nsa.me.uk/pose";setInterval(b,1500)});';
 var a = document.body.appendChild(s);
 console.log(a);
 
@@ -20,16 +20,19 @@ console.log(a);
 
 //         function findTextBoxes (argument) {
 //                 var boxes = $('.cke_enable_context_menu');
-//                 var parent = boxes[0].parentNode;
-//                 $(parent).prepend("<i id=\"SmartyFace\" style=\"pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">Type Reply Here</i>");
-//                 var textarea = boxes[0];
-//                 $(boxes[0]).keypress(function(a) {
-//                         var text = $(textarea).val();
-//                         $.post( endPoint, { post: text }, function( data ) {
-//                                 $('#SmartyFace').html( $(textarea).val()+data );
+//                 if(!boxes[0]) {
+//                         setInterval(findTextBoxes,1500);
+//                 } else {
+//                         var parent = boxes[0].parentNode;
+//                         $(parent).prepend("<i id=\"SmartyFace\" style=\"pointer-events:none; color: #CCC;position: absolute;font: 13px Verdana,Arial,Tahoma,Calibri,Geneva,sans-serif;padding: 0 1px 0 1px;\">Type Reply Here</i>");
+//                         var textarea = boxes[0];
+//                         $(boxes[0]).keypress(function(a) {
+//                                 var text = $(textarea).val();
+//                                 $.post( endPoint, { post: text }, function( data ) {
+//                                         $('#SmartyFace').html( $(textarea).val()+data );
+//                                 });
 //                         });
-//                 });
+//                 }
 //         }
 //         setInterval(findTextBoxes,1500);
-//         setInterval(findTextBoxes,15000);
 // });
